29e53e1b208bdbb49c39fce2c3ec6cb2c55a3a9f
Efe Kaan Aslan <148584915+XPR01423@users.noreply.github.com>
2026-03-10 13:52:14 +0300

    Delete rootfs directory

--- rootfs/system/include/generated/compile.h
+++
@@ -1,4 +0,0 @@
-#define UTS_MACHINE		"x86_64"
-#define LINUX_COMPILE_BY	"efe"
-#define LINUX_COMPILE_HOST	"cachyos-x8664"
-#define LINUX_COMPILER		"gcc (GCC) 15.2.1 20251112, GNU ld (GNU Binutils) 2.45.1"
